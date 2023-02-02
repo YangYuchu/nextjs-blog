b0899d3c85b1fb73900c338da06f0299ae604cda
Yuchu Yang <yangyuchu0419@gmail.com>
2023-02-03 00:45:02 +0900

    Completed Next.js example code

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+// import global css from nextjs-blog\styles\global.css
+import "../styles/global.css";
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
